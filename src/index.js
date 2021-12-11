85ea4fc36058659c30dbbf8e89dd91e2909e9903
Borjamrd <borjamrd1@gmail.com>
2021-12-11 14:32:15 +0100

    bottons abrir noticia working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Components/styles.css';
-import new1 from './Components/images/new1.jpg';
-import new2 from '.Components/images/new2.webp';
-import new3 from '.Components/images/new3.jpg';
-import logo from '.Components/images/elpais.svg'
+
+/* import new1 from './Components/images/new1.jpg'; */
+// import new2 from '.Components/images/new2.webp';
+// import new3 from '.Components/images/new3.jpg';
+// import logo from '.Components/images/elpais.svg';
 
 
-const data = {logo: logo, name: "El País", email: "contacto@elpais.es", phone: "+34 722 11 21   11", language: "Español"};
+const data = {/* logo: logo, */ name: "El País", email: "contacto@elpais.es", phone: "+34 722 11 21   11", language: "Español"};
 
 const writer1 = {
   name: "Miguel Jimenez", 
@@ -34,9 +35,9 @@ const writer3 = {
 }
 
 const socialMedia=[
-  {href:"", name: "facebook", icon:""},
-  {href:"", name: "instagram", icon:""},
-  {href:"", name: "twitter", icon:""}
+  {href:"", name: "facebook"},
+  {href:"", name: "instagram"},
+  {href:"", name: "twitter"}
 ]
 
 const INTERNACIONAL = 1;
@@ -61,7 +62,7 @@ const categories = [INTERNACIONAL, OPINION, ESPANA,ECONOMIA, SOCIEDAD, EDUCACION
 const news = [
       {title: "Diego Ávalos, jefe de Netflix en España: “Nos interesa el talento en cualquier idioma",
       subtitle: "El vicepresidente de la plataforma de series en la Península afirma que el compromiso con el país es “firme”, al margen de la nueva ley audiovisual",
-      newsImage: new1,
+      // newsImage: new1,
       writer: writer1.name,
       category: ECONOMIA,
       location: writer1.location,
@@ -72,7 +73,7 @@ const news = [
 
       {title: "Hacienda recurre la sentencia que le obliga a devolver más de 1.000 millones a Telefónica",
       subtitle: "El fisco acude al Supremo y consigue de momento demorar el desembolso",
-      newsImage: new2,
+      // newsImage: new2,
       writer: writer2.name,
       location: writer2.location,
       category: INTERNACIONAL,
@@ -83,7 +84,7 @@ const news = [
      
       {title: "Marta Ortega, heredera del imperio de Zara y nueva mujer fuerte del Ibex",
       subtitle: "La hija del fundador de Inditex, que se convertirá en presidenta del grupo en abril, ha sido clave en el desarrollo de las colecciones y la imagen de la empresa",
-      newsImage: new3,
+      /* newsImage: new3, */
       writer: writer3.name,
       location: writer3.location,
       category: SOCIEDAD,
@@ -93,10 +94,11 @@ const news = [
       content: "Marta Ortega Pérez no tiene un despacho en Zara a pesar de que lleva trabajando en la empresa 15 años. La hija pequeña de Amancio Ortega, de 37 años, que a partir del 1 de abril se pondrá al frente del imperio que levantó su padre como presidenta no ejecutiva, prefiere trabajar en un espacio abierto, codo con codo con el resto del equipo, y visitar casi todas las semanas las tiendas de la marca para estar cerca del corazón del negocio. En este sentido recuerda a su padre, que nunca ha tenido una oficina separada en la sede del grupo en Arteixo (A Coruña). Pero los que la conocen están convencidos de que el estilo de gestión de la futura presidenta de Inditex estará más alejado del gran hermetismo que caracterizó al fundador del grupo. Una muestra de este cambio fue la extensa entrevista que The Wall Street Journal publicó en septiembre, la primera que Marta Ortega ha concedido (su padre nunca dio ninguna y durante años fue difícil verlo y fotografiarlo en público). “Estaré siempre donde la compañía más me necesite”, afirmaba la empresaria gallega en lo que ahora se interpreta como un anticipo de su nuevo papel en el grupo, confirmado este martes. En la entrevista con el Journal, diario de cabecera de la élite financiera, recordaba sus inicios trabajando en una tienda de Zara en King’s Road, en el barrio londinense de Chelsea. “La primera semana pensé que no iba a sobrevivir”, aseguraba. “Pero luego es como si te hicieras adicta a la tienda. Hay gente que nunca quiere irse”, comentaba en el artículo. Lo que no cuenta en esta entrevista es que, cuando fue a trabajar de dependienta sin revelar que era la hija del jefe, sus compañeras pronto se enteraron al ver el Rolex que llevaba."},
   ]
 
+  
 
 
 const navBarElements = [
-  {href: "", name: ECONOMIA},
+  {href: "", name: "Economía"},
   {href: "", name: "Opinion"},
   {href: "", name: "España"},
   {href: "", name: "Economia"},
@@ -118,9 +120,10 @@ const navBarElements = [
 class Header extends React.Component{
   constructor(props) {
     super(props)
+    this.state = {...this.props.categories}
   }
   render(){
-    return <div>
+    return <div className="header">
       <div className="title-header">
         <h1>{data.name}</h1>
         <ul>
@@ -128,7 +131,7 @@ class Header extends React.Component{
           <li>{data.phone}</li>
         </ul>
       </div>
-      <NavBar/>
+      <NavBar className='navbar' elements={navBarElements}/>
     </div>
   }
 }
@@ -136,48 +139,39 @@ class Header extends React.Component{
 class NavBar extends React.Component{
   constructor(props) {
     super(props)
-    this.state={...this.props.categories}
   }
   render(){
+    let navBar = this.props.elements.map((element) => <div className='navbarelement'><a href ={element.href}>{element.name}</a></div>)
       return <nav>
-           <NavBarElement elements={navBarElements} />
+           {navBar}
     </nav> 
   }
 }
 
 
-class NavBarElement extends React.Component{
-  
-  render(){
-    let navBarElement = this.props.elements.map((element) => <a href ={element.href}>{element.name}</a>)
-      return <>
-          {navBarElement}
-      </> 
-  }
-}
-
 class Newspaper extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {isNewOpen: false}
+    this.state = {isNewOpen: false, isNewspaperOpen: true}
     this.openNews = this.openNews.bind(this)
   }
 
   openNews(){
     this.setState(state => ({isNewOpen : !state.isNewOpen})) 
   }
+  
 
   render(){
     let myRender = undefined
     if (this.state.isNewOpen === true) {
       myRender = <div>
        <Header />
-       <New/>
+       <New openNews={this.openNews}/>
        <Footer/>
       </div>
     } else {
       myRender=  <div><Header />
-      <Board news={news} openNews={this.openNews}/>  /* invoca la 167 para terminar de elevar el estado */
+      <Board news={news} openNews={this.openNews}/>
       <Footer/>
       </div>
     }
@@ -190,15 +184,14 @@ class Newspaper extends React.Component{
 class Board extends React.Component{
   constructor(props){
     super(props)
-    this.state={...news}
+    this.state={...news, isNewOpen: true}
     this.openNews =this.openNews.bind(this);
     
   }
+
   openNews(){
     this.props.openNews()
   }
- 
- 
     render(){
       let newsItem = this.props.news.map((element) =><New elements={element} openNews={this.openNews} />);  /* pasar via propiedades por la linea 199 */
       return <div>
@@ -211,22 +204,30 @@ class New extends React.Component{
   constructor(props){
     super(props)
     this.openNews = this.openNews.bind(this)
-    this.state = {...this.props.elements, isNewOpen: false}
+    this.state = {...this.props.elements, isNewOpen: false, isNewspaperOpen: true}
   }
  
   openNews(){
-    this.setState(state =>(
-        {isNewOpen: !state.isNewOpen}
-    ));
     this.props.openNews();  /* luego esto bindea openNews como propiedad a a la linea 204 */
   }
   render() {
+    let myButton = undefined;
+    if(this.state.isNewOpen){
+      myButton = <div>
+        <button onClick={this.openNews} > ATRÁS</button>
+      </div>
+    }else{
+      myButton= <div>
+        <button onClick={this.openNews} > ABRIR NOTICIA</button>
+        </div>
+    }
             
-  return <div onClick={this.openNews} >  /* esto va a bindiar con la linea 218 */
-      <img src={this.state.newsImage} alt ="other form of visual" className="new-image"></img>
+  return <div className='new'>
+      {/* <img src={this.state.newsImage}  className="new-image"></img> */}
           <h1>{this.state.title}</h1>
           <h4>{this.state.subtitle}</h4>
           <p>{this.state.content}</p>
+          {myButton}
       </div>
   }
 }
@@ -234,7 +235,10 @@ class New extends React.Component{
 
 
 class Footer extends React.Component {
-
+  constructor(props){
+    super(props)
+    this.state = {...this.props.socialMedia}
+  }
   render(){
     return<>
     <NavBar elements={socialMedia}/>
